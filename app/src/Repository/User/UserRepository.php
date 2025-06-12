42ec46bbb3acf44c7ca59a841e972b896dd4daa0
MD. ATIKUR RAHMAN <golenur02@gmail.com>
2025-06-12 16:04:33 +0600

    UserRepository class refactor

--- app/src/Repository/User/UserRepository.php
+++ app/src/Repository/User/UserRepository.php
@@ -4,23 +4,24 @@ namespace App\Repository\User;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
 class UserRepository  extends ServiceEntityRepository implements UserRepositoryInterface
 {
+    private readonly EntityManagerInterface $entityManager;
 
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
+        $this->entityManager = $this->getEntityManager();
     }
 
     public function save(User $user, bool $flush = true): void
     {
-        $entityManager = $this->getEntityManager(); // Proper accessor
-
-        $entityManager->persist($user);
+        $this->entityManager->persist($user);
         if ($flush) {
-            $entityManager->flush();
+            $this->entityManager->flush();
         }
     }
 }
