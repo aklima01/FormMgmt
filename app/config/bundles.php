9943f82d6e1d0cb7a4e9d84f996cee4ff53cad0a
MD. ATIKUR RAHMAN <golenur02@gmail.com>
2025-06-15 17:32:06 +0600

    delete unused files

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -14,6 +14,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
